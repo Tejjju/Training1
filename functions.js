60af70f03053b7701f060d9b99ce5121aac74931
Tejjju <2211cs010629@mallareddyuniversity.ac.in>
2025-01-09 11:30:03 +0530

    HI

---
+++ functions.js
@@ -0,0 +1,29 @@
+//function userDetails(name,age,city){
+    //console.log(name);
+    //console.log(age);
+    //console.log(city);
+//}
+//userDetails("Tejaswini",20,"Guntur");
+
+
+//(function(){
+    //console.log("IIFE");
+//})();
+
+
+let x =_=>console.log("Arrow function");
+x();
+
+let y =()=>console.log("Hello") 
+           console.log("How are you ?");
+y();
+
+let u =()=>{console.log("Hello") 
+           console.log("How are you ?");};
+u();
+
+let z =(a,b)=>console.log(a+b);
+z(5,4);
+
+let n =(a,b)=>console.log(a+b);
+n(5);
