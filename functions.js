5e0dfe823d8866d0b7acd3ae16d1d84bff38656a
Tejjju <2211cs010629@mallareddyuniversity.ac.in>
2025-01-09 11:53:34 +0530

    patterns

--- functions.js
+++ functions.js
@@ -1,14 +1,14 @@
-//function userDetails(name,age,city){
-    //console.log(name);
-    //console.log(age);
-    //console.log(city);
-//}
-//userDetails("Tejaswini",20,"Guntur");
+function userDetails(name,age,city){
+    console.log(name);
+    console.log(age);
+    console.log(city);
+}
+userDetails("Tejaswini",20,"Guntur");
 
 
-//(function(){
-    //console.log("IIFE");
-//})();
+(function(){
+    console.log("IIFE");
+})();
 
 
 let x =_=>console.log("Arrow function");
@@ -27,3 +27,54 @@ z(5,4);
 
 let n =(a,b)=>console.log(a+b);
 n(5);
+
+
+
+  
+  
+ 
+  function printCustomPattern(rows, cols) {
+      let pattern = "";
+      for (let i = 0; i < rows; i++) {
+          for (let j = 0; j < cols; j++) {
+              pattern += "*  ";
+          }
+          pattern += "\n";
+      }
+      console.log(pattern);
+  }
+  printCustomPattern(5,5);
+  
+
+  function printTrianglePattern(rows) {
+    let pattern = "";
+    for (let i = 1; i <= rows; i++) { 
+      for (let j = 1; j <= i; j++) { 
+        pattern += "* ";
+      }
+      pattern += "\n"; 
+    }
+    console.log(pattern);
+  }
+  
+  printTrianglePattern(5);
+
+
+  function printDiagonalPattern(size) {
+    let pattern = "";
+    for (let i = 0; i < size; i++) {
+      for (let j = 0; j < size; j++) {
+        if (i === j || i + j === size - 1) {
+          pattern += "* ";
+        } else {
+          pattern += "  "; 
+        }
+      }
+      pattern += "\n";
+    }
+    console.log(pattern);
+  }
+  
+
+  printDiagonalPattern(5); 
+   
