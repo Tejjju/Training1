2123ec16ce2e3f514b401d9811ffb490c3a9dfdf
Tejjju <2211cs010629@mallareddyuniversity.ac.in>
2025-01-11 12:31:18 +0530

    console

---
+++ function_1.js
@@ -0,0 +1,20 @@
+// function x() {
+//     let a = 10;
+//     const b = 20;
+//     console.log(a,b);
+//     function y() {
+//         let p = "Js";
+//         var q = "ReactJs";
+//         console.log(p,q);
+//         function z() {
+//             const username = "Tejaswini";
+//             console.log(username);
+//             console.log(a);
+//             console.log(q);
+//         }
+//         z();
+//     }
+//     y();
+// }
+// x();
+
