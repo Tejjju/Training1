330262b6fc7b1feaae7da48e1bc8aa6eb6845b95
Tejjju <2211cs010629@mallareddyuniversity.ac.in>
2025-01-22 13:02:08 +0530

    ApiFetch

--- async.js
+++ async.js
@@ -1,8 +1,8 @@
 // console.log(10);
 // console.log(20);
-// // setTimeout(()=>{
-// //     console.log(30);
-// // },5000)
+// setTimeout(()=>{
+//     console.log(30);
+// },5000)
 
 // setInterval(()=>{
 //     // document.writeln("MRU");
@@ -14,25 +14,52 @@
 // let p1 = new Promise((resolve, reject)=>{});
 // console.log(p1);
 
-let p2 = new Promise((resolve, reject)=>{
-    resolve("success");
-})
-// console.log(p2);
-p2
-.then((response)=>console.log(response))
-p2.
-catch((error)=>console.log(error))
-p2.
-finally(()=>console.log("finally for both"))
+// let p2 = new Promise((resolve, reject)=>{
+//     resolve("success");
+// })
+// // console.log(p2);
+// p2
+// .then((response)=>console.log(response))
+// p2.
+// catch((error)=>console.log(error))
+// p2.
+// finally(()=>console.log("finally for both"))
 
 
-let p3 = new Promise((resolve, reject)=>{
-    reject("failure");
-})
-console.log(p3);
-p3
-.then((response)=>console.log(response))
-p3
-.catch((error)=>console.log(error))
-p3
-.finally(()=>console.log("finally for both"))
+// let p3 = new Promise((resolve, reject)=>{
+//     reject("failure");
+// })
+// console.log(p3);
+// p3
+// .then((response)=>console.log(response))
+// p3
+// .catch((error)=>console.log(error))
+// p3
+// .finally(()=>console.log("finally for both"))
+
+
+function fetchUsers(){
+    let response = fetch("https://jsonplaceholder.typicode.com/users");
+    // console.log(response);
+    response.then(result=>{
+        // console.log(result.json());
+        return result.json().then(data=>{
+            console.log(data);
+            let store = document.getElementById("store");
+            console.log(store);
+            data.map(user=>{
+                store.innerHTML +=
+                `<tr>
+                    <td>${user.id}</td>
+                    <td>${user.name}</td>
+                    <td>${user.email}</td>
+                    <td>${user.company.name}</td>
+                </tr>`
+            })
+
+        })
+    })
+    .catch(err=>console.log(err))
+}
+fetchUsers();
+
