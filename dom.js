1e55e0f5d9e3c76f1e6ce060ebac2cf28c2f7e2f
Tejjju <2211cs010629@mallareddyuniversity.ac.in>
2025-01-27 13:05:48 +0530

    IMAGE

--- dom.js
+++ dom.js
@@ -31,48 +31,92 @@
 //     console.log("button clicked");
 // })
 
-const divs = document.querySelectorAll('div');
+// const divs = document.querySelectorAll('div');
 
 
-divs.forEach(div => {
+// divs.forEach(div => {
 
-    div.addEventListener('mouseover', () => {
-        div.style.backgroundColor = div.textContent;
-    });
-    div.addEventListener('mouseleave', () => {
-        div.style.backgroundColor = ''; 
-    });
-});
+//     div.addEventListener('mouseover', () => {
+//         div.style.backgroundColor = div.textContent;
+//     });
+//     div.addEventListener('mouseleave', () => {
+//         div.style.backgroundColor = ''; 
+//     });
+// });
 
 
-let ele = document.createElement("h1");
-ele.innerText = "Dynamic";
-// ele.setAttribute("id", "demo");
-ele.id = "demo";
-// console.log(ele);
+// let ele = document.createElement("h1");
+// ele.innerText = "Dynamic";
+// // ele.setAttribute("id", "demo");
+// ele.id = "demo";
+// // console.log(ele);
+
+// let image = document.createElement("img");
+// image.src = "https://th.bing.com/th/id/OIG1.wQ7nqzXG6LLji1s3MrOP";
+// console.log(image);
+
+// document.body.appendChild(ele);
+// document.body.appendChild(image);
+
+// let form = document.querySelector("form");
+// let username = document.getElementById("uName");
+// let mail = document.getElementById("uMail");
+// let psw = document.getElementById("uPass");
+
+// form.addEventListener("submit", (event)=>{
+//     event.preventDefault();
+//     console.log("form submitted");
+//     let uname = username.value;
+//     let uemail = mail.value;
+//     let upass = psw.value;
+//     let userDetails={
+//         uname, uemail, upass
+//     }
+//     console.log(userDetails);
+//     localStorage.setItem("userData", JSON.stringify(userDetails))
+
+// })
+
+// let video = document.createElement("video");
+// video.setAttribute("src", "")
+// console.log(video);
+
+let mainEle = document.createElement("div");
+mainEle.setAttribute("id", "mainBlock");
+mainEle.style.border="2px solid black";
+mainEle.style.width="1000px";
+mainEle.style.height="1000px";
+
+console.log(mainEle);
+
+let topEle = document.createElement("div");
+topEle.setAttribute("class","topBlock");
+// console.log(topEle);
 
 let image = document.createElement("img");
 image.src = "https://th.bing.com/th/id/OIG1.wQ7nqzXG6LLji1s3MrOP";
-console.log(image);
+image.style.width="1000px"
 
-document.body.appendChild(ele);
-document.body.appendChild(image);
+let bottomEle = document.createElement("div");
+bottomEle.setAttribute("class", "bottomBlock");
 
-let form = document.querySelector("form");
-let username = document.getElementById("uName");
-let mail = document.getElementById("uMail");
-let psw = document.getElementById("uPass");
 
-form.addEventListener("submit", (event)=>{
-    event.preventDefault();
-    console.log("form submitted");
-    let uname = username.value;
-    let uemail = mail.value;
-    let upass = psw.value;
-    let userDetails={
-        uname, uemail, upass
-    }
-    console.log(userDetails);
-    localStorage.setItem("userData", JSON.stringify(userDetails))
+let h1 = document.createElement("h1");
+h1.innerText = "MAGIC";
+h1.style.textAlign= "center";
 
-})
+let btn = document.createElement("button");
+btn.innerText="View More";
+btn.style.border="none";
+btn.style.padding="20px";
+btn.style.backgroundColor="dodgerblue";
+btn.style.color="white";
+
+bottomEle.appendChild(h1);
+bottomEle.appendChild(btn);
+
+topEle.appendChild(image);
+mainEle.appendChild(topEle);
+mainEle.appendChild(bottomEle);
+document.body.appendChild(mainEle);
+
